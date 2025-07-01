feb2ed895570d7c606e8166639a8a258340c5f1b
MohamedRouias <146641169+MohamedRouias@users.noreply.github.com>
2025-07-01 08:47:13 +0000

    REGISTER Y LOGIN ACABADOS. (hay algún que otro añadido)

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from datetime import datetime
+from functools import wraps
 
 # from src.api.models import db
 # from flask import Flask
@@ -19,7 +20,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-# from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
 
@@ -30,7 +31,7 @@ static_file_dir = os.path.join(os.path.dirname(
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-# bcrypt = Bcrypt(app) #para encriptar
+bcrypt = Bcrypt(app)  # para encriptar
 
 
 app.url_map.strict_slashes = False
@@ -88,10 +89,10 @@ def serve_any_other_file(path):
     return response
 
 
-
 # ---------GET USERS ---OK----------------------------------------------
 
 @app.route('/users', methods=['GET'])
+@jwt_required()
 def get_users():
     users = User.query.all()
     print(users)
@@ -101,24 +102,27 @@ def get_users():
     return jsonify({'msg': 'ok', 'results': user_serialized}), 200
 
 
-#---------GET by id USERS ---OK ---------------------------------------
+# ---------GET by id USERS ---OK ---------------------------------------
 
 @app.route('/users/<int:id>', methods=['GET'])
+@jwt_required()
 def get_user_by_id(id):
-    user= User.query.get(id)# query.get solo funciona para devolver primary key. para devolver otro campo usar query.filter_by
-    print (user)
+    # query.get solo funciona para devolver primary key. para devolver otro campo usar query.filter_by
+    user = User.query.get(id)
+    print(user)
     if user is None:
-        return jsonify ({'msg': 'Usuario no encontrado'}), 404
+        return jsonify({'msg': 'Usuario no encontrado'}), 404
     return jsonify({'msg': 'ok', 'result': user.serialize()}), 200
 
 
 # ---------POST USERS ---OK----------------------------------------------
 
 @app.route('/users', methods=['POST'])
+@jwt_required()
 def post_user():
     body = request.get_json(silent=True)
 
-    required_fields= [ 'email', 'password', 'name', 'rol', 'is_active']
+    required_fields = ['email', 'password', 'name', 'rol', 'is_active']
     if not all(field in body for field in required_fields):
         return jsonify({'msg': 'Some fields are missing to fill'}), 400
 
@@ -141,136 +145,160 @@ def post_user():
     return jsonify({'msg': 'Usuario creado correctamente', 'user': new_user.serialize()}), 201
 
 
-#----------------------------------GET TODAS LAS COMANDAS ---OK--------------------------
+# ----------------------------------GET TODAS LAS COMANDAS ---OK--------------------------
 @app.route('/orders', methods=['GET'])
+@jwt_required()
 def get_orders():
-    orders= Orders.query.all()
-    print (orders)
+    orders = Orders.query.all()
+    print(orders)
     user_serialized = []
     for order in orders:
         user_serialized.append(order.serialize())
-    return jsonify({'msg': 'ok', 'results' : user_serialized}), 200
+    return jsonify({'msg': 'ok', 'results': user_serialized}), 200
 
-#-------------------------------GET DE UNA COMANDA ---OK--------------------------------
+# -------------------------------GET DE UNA COMANDA ---OK--------------------------------
+
+
 @app.route('/orders/<int:id>', methods=['GET'])
+@jwt_required()
 def get_order_by_id(id):
-    order= Orders.query.get(id)# query.get solo funciona para devolver primary key. para devolver otro campo usar query.filter_by
-    print (order)
+    # query.get solo funciona para devolver primary key. para devolver otro campo usar query.filter_by
+    order = Orders.query.get(id)
+    print(order)
     if order is None:
-        return jsonify ({'msg': 'Comanda no encontrada'}), 404
-    return jsonify({'msg': 'ok', 'result':order.serialize()}), 200
+        return jsonify({'msg': 'Comanda no encontrada'}), 404
+    return jsonify({'msg': 'ok', 'result': order.serialize()}), 200
 
-#-------------------------------POST DE COMANDAS ---OK------------------------------------ 
+# -------------------------------POST DE COMANDAS ---OK------------------------------------
+
+
 @app.route('/orders', methods=['POST'])
+@jwt_required()
 def crear_comanda():
-    body= request.get_json(silent=True)
-   
-                    
+    body = request.get_json(silent=True)
+
     if body is None:
-        return jsonify ({'msg': 'Debes enviar informacion'}), 404
-    if 'mesa_id' not in body: 
-        return jsonify ({'msg': 'Debes introducir el numero de la mesa en el campo "mesa_id"'}), 404
-    if 'usuario_id' not in body: 
-        return jsonify ({'msg': 'Debes introducir el usuario que atiende la comanda en el campo "usuario_id" '}), 404
-        
+        return jsonify({'msg': 'Debes enviar informacion'}), 404
+    if 'mesa_id' not in body:
+        return jsonify({'msg': 'Debes introducir el numero de la mesa en el campo "mesa_id"'}), 404
+    if 'usuario_id' not in body:
+        return jsonify({'msg': 'Debes introducir el usuario que atiende la comanda en el campo "usuario_id" '}), 404
+
     try:
-    
-         new_order = Orders() # nuevo_comanda es una instancia de la clase 
-         new_order.mesa_id= body['mesa_id']
-         new_order.usuario_id= body['usuario_id']
-         new_order.state= EstadoComanda['pendiente'] #por defecto la inicializo en pendiente 
-         new_order.total_price= 0
-         if 'guest_notes' in body:
-             new_order.guest_notes= body['guest_notes']
-         db.session.add(new_order)
-         db.session.flush()  # para obtener el ID sin hacer commit aún
-         
-         total=0
-         print(body['platos'])
-         for item in body['platos']:
+
+        new_order = Orders()  # nuevo_comanda es una instancia de la clase
+        new_order.mesa_id = body['mesa_id']
+        new_order.usuario_id = body['usuario_id']
+        # por defecto la inicializo en pendiente
+        new_order.state = EstadoComanda['pendiente']
+        new_order.total_price = 0
+        if 'guest_notes' in body:
+            new_order.guest_notes = body['guest_notes']
+        db.session.add(new_order)
+        db.session.flush()  # para obtener el ID sin hacer commit aún
+
+        total = 0
+        print(body['platos'])
+        for item in body['platos']:
             plato_id = item.get('plate_id')
             cantidad = item.get('cantidad', 1)
             if plato_id is None:
                 continue
-            plato = Plates.query.get(plato_id) #instancio platos 
+            plato = Plates.query.get(plato_id)  # instancio platos
 
             # Crear relación Orders_Plates
-            new_order_plate = Orders_Plates() #instancio Order_Plates
-            new_order_plate.plate_id=plato_id
-            new_order_plate.order_id=new_order.id
-            new_order_plate.count_plat=cantidad
-            
+            new_order_plate = Orders_Plates()  # instancio Order_Plates
+            new_order_plate.plate_id = plato_id
+            new_order_plate.order_id = new_order.id
+            new_order_plate.count_plat = cantidad
+
             db.session.add(new_order_plate)
 
             # Calcular precio total
             total += float(plato.price) * cantidad
 
-         new_order.total_price = total
-         db.session.commit()
+        new_order.total_price = total
+        db.session.commit()
 
-         return jsonify({'msg': 'Comanda creada exitosamente', 'result': new_order.serialize()}), 201
-    
+        return jsonify({'msg': 'Comanda creada exitosamente', 'result': new_order.serialize()}), 201
+
     except Exception as e:
         db.session.rollback()
         return jsonify({'msg': f'Error al crear la comanda: {str(e)}'}), 500
-    
-#-----------------------------PUT DE COMANDA -------------------OJOOO--------------------------------------
-#falta actualizar sin borrar los anteriores platos 
+
+# -----------------------------PUT DE COMANDA -------------------OJOOO--------------------------------------
+# falta actualizar sin borrar los anteriores platos
+
+
 @app.route('/orders/<int:order_id>', methods=['PUT'])
+@jwt_required()
 def update_orders(order_id):
     body = request.get_json(silent=True)
     update_order = Orders.query.get(order_id)
-    if body is None: 
+    if body is None:
         return jsonify({'msg': 'Debe introducir los elementos de la comanda a modifiar!'}), 404
-    
+
     if 'state' in body:
         try:
             update_order.state = EstadoComanda(body['state'])
         except ValueError:
             return jsonify({'msg': f'Estado no válido!!'}), 404
-        
+
     if 'mesa_id' in body:
         mesa = Tables.query.get(body['mesa_id'])
         if mesa is None:
-            return jsonify({'msg':'Mesa no existe!!'}),404
-        update_order.mesa_id = body['mesa_id'] 
+            return jsonify({'msg': 'Mesa no existe!!'}), 404
+        update_order.mesa_id = body['mesa_id']
 
     if 'usuario_id' in body:
         user = User.query.get(body['usuario_id'])
         if user is None:
-            return jsonify({'msg':'Usuario inexistente!!'}),404
+            return jsonify({'msg': 'Usuario inexistente!!'}), 404
         update_order.usuario_id = body['user_id']
 
     if 'guest_notes' in body:
-        update_order.guest_notes= body['guest_notes']
+        update_order.guest_notes = body['guest_notes']
 
     print(body['platos'])
     if 'platos' in body:
         try:
             # Eliminar platos anteriores de esa comanda
-            #Orders_Plates.query.filter_by(order_id=update_order.id).delete()
-            Orders_Plates.query.filter_by(order_id=update_order.id)
+            # Orders_Plates.query.filter_by(order_id=update_order.id).delete()
+            # Orders_Plates.query.filter_by(order_id=update_order.id)
+
             total = 0
             for item in body['platos']:
-                 plato_id = item.get('plate_id')
-                 cantidad = item.get('cantidad', 1)
-                 if plato_id is None:
+                plato_id = item.get('plate_id')
+                cantidad = item.get('cantidad', 1)
+                if plato_id is None:
                     continue
 
-                 plato = Plates.query.get(plato_id)
-                 if not plato:
+                plato = Plates.query.get(plato_id)
+                if not plato:
                     continue
 
-                 new_order_plate = Orders_Plates()
-                 new_order_plate.plate_id=plato_id
-                 new_order_plate.order_id=update_order.id,
-                 new_order_plate.count_plat=cantidad
+                # Verificamos que exista el plato en la comanda
+                plato_existente = Orders_Plates.query.filter_by(
+                    order_id=update_order.id, plate_id=plato_id).first()
 
-                 db.session.add(new_order_plate)
-                 total += float(plato.price) * cantidad
- 
+                if plato_existente:
+                    plato_existente.count_plat = cantidad
+                    print(plato_existente)
+                else:
+                    new_order_plate = Orders_Plates()
+                    new_order_plate.plate_id = plato_id
+                    new_order_plate.order_id = update_order.id
+                    new_order_plate.count_plat = cantidad
+                    db.session.add(new_order_plate)
+                    db.session.commit()
+
+                total += float(plato.price) * cantidad
+
+# ELIMINAR DE LA TABLA SI LA CANTIDAD LLEGA A 0! (ACORDARSE, DE COCINA TAMPOCO DEBERÍA VERSE)
+# ALERTAR AL USUARIO SI DESEA ELIMINARLO 
+
+
             update_order.total_price = total
-                   
 
         except Exception as e:
             db.session.rollback()
@@ -283,21 +311,25 @@ def update_orders(order_id):
     except Exception as e:
         db.session.rollback()
         return jsonify({'msg': 'Error al actualizar la comanda', 'error': str(e)}), 500
-  
-    
-#--------------------ELIMINAR UNA COMANDA CON EL ID ----OK------
-@app.route('/orders/<int:order_id>', methods = ['DELETE'])
+
+
+# --------------------ELIMINAR UNA COMANDA CON EL ID ----OK------
+@app.route('/orders/<int:order_id>', methods=['DELETE'])
+@jwt_required()
 def eliminar_comanda_por_id(order_id):
-    order= Orders.query.get(order_id) # duda , aqui solo obtengo el id o toda la instancia 
-    if order is None :
+    # duda , aqui solo obtengo el id o toda la instancia
+    order = Orders.query.get(order_id)
+    if order is None:
         return jsonify({'msg': f'no existe la comanda con id {order_id}'}), 400
     db.session.delete(order)
     db.session.commit()
-    return jsonify({'msg':'ok', 'results': f'La comanda con id {order_id} perteneciente a la mesa {order.mesa_id} ha sido borrado dela lista de comandas'}), 200
- 
- 
- #-------------------ELIMINAR TODAS LAS COMANDAS DE UNA MESA--- OK ----------------------
+    return jsonify({'msg': 'ok', 'results': f'La comanda con id {order_id} perteneciente a la mesa {order.mesa_id} ha sido borrado dela lista de comandas'}), 200
+
+ # -------------------ELIMINAR TODAS LAS COMANDAS DE UNA MESA--- OK ----------------------
+
+
 @app.route('/orders/table/<int:mesa_id>', methods=['DELETE'])
+@jwt_required()
 def eliminar_comanda_por_mesa_id(mesa_id):
     # Buscar todas las comandas asociadas a esa mesa
     orders = Orders.query.filter_by(mesa_id=mesa_id).all()
@@ -314,33 +346,36 @@ def eliminar_comanda_por_mesa_id(mesa_id):
 
     except Exception as e:
         db.session.rollback()
-        return jsonify({'msg': f'Error eliminando las comandas: {str(e)}'}), 500   
-    
+        return jsonify({'msg': f'Error eliminando las comandas: {str(e)}'}), 500
 
 
-
-#-------------------------------GET PLATOS ---OK----------------------------------------
+# -------------------------------GET PLATOS ---OK----------------------------------------
 @app.route('/plates', methods=['GET'])
+@jwt_required()
 def get_plates():
-    plates= Plates.query.all()
-    print (plates)
+    plates = Plates.query.all()
+    print(plates)
     plates_serialized = []
     for plates in plates:
         plates_serialized.append(plates.serialize())
-    return jsonify({'msg': 'ok', 'results' : plates_serialized}), 200
+    return jsonify({'msg': 'ok', 'results': plates_serialized}), 200
 
-#-----------------------------GET UN PLATO POR id ---OK-----------------------------
+# -----------------------------GET UN PLATO POR id ---OK-----------------------------
+
+
 @app.route('/plates/<int:id>', methods=['GET'])
+@jwt_required()
 def get_plate_by_id(id):
-    plates= Plates.query.get(id)
-    print (plates)
+    plates = Plates.query.get(id)
+    print(plates)
     if plates is None:
-        return jsonify ({'msg': 'Plato no encontrado'}), 404
+        return jsonify({'msg': 'Plato no encontrado'}), 404
     return jsonify({'msg': 'ok', 'result': plates.serialize()}), 200
 
 
 # -------------------------------GET DE TABLES ---OK --------------------------------
 @app.route('/tables', methods=['GET'])
+@jwt_required()
 def get_tables():
     tables = Tables.query.all()
     print(tables)
@@ -352,6 +387,7 @@ def get_tables():
 
 # -------------------------------GET DE UNA TABLE ID ---OK--------------------------------
 @app.route('/tables/<int:table_id>', methods=['GET'])
+@jwt_required()
 def get_table_by_id(table_id):
     table = Tables.query.filter_by(id=table_id).first()
     print(table)
@@ -365,7 +401,7 @@ def get_table_by_id(table_id):
 def update_tables(table_id):
     body = request.get_json(silent=True)
     table = Tables.query.get(table_id)
-    if body is None: #***decia table
+    if body is None:  # ***decia table
         return jsonify({'msg': 'Mesa no encontrada!'}), 404
     if 'state' in body:
         try:
@@ -373,11 +409,11 @@ def update_tables(table_id):
         except ValueError:
             return jsonify({'msg': f'Estado no válido!!'}), 404
     if 'seats' in body:
-        table.seats = body['seats'] #****decia table.state
+        table.seats = body['seats']  # ****decia table.state
     if 'user_id' in body:
         user = User.query.get(body['user_id'])
         if user is None:
-            return jsonify({'msg':'Usuario inexistente!!'}),404
+            return jsonify({'msg': 'Usuario inexistente!!'}), 404
         table.user_id = body['user_id']
     try:
         db.session.commit()
@@ -386,6 +422,96 @@ def update_tables(table_id):
         db.session.rollback()
         return jsonify({'msg': 'Error al actualizar la mesa', 'error': str(e)}), 500
 
+# -------------------------------REGISTER ---OK--------------------------------
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar informarmación en el body'}), 400
+    if 'name' not in body:
+        return jsonify({'msg': 'El campo name es obligatorio'}), 400
+    if 'rol' not in body:
+        return jsonify({'msg': 'El campo rol es obligatorio'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'El campo email es obligatorio'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'El campo password es obligatorio'}), 400
+    new_user = User()
+    new_user.name = body['name']
+    new_user.rol = body['rol']
+    new_user.email = body['email']
+    new_user.password = bcrypt.generate_password_hash(
+        body['password']).decode('utf-8')
+    new_user.is_active = True
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'msg': f'Usuario {new_user.name} CREADO'}), 201
+
+
+# -------------------------------LOGIN ---OK--------------------------------
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar informarmación en el body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'El campo email es obligatorio'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'El campo password es obligatorio'}), 400
+    
+    user = User.query.filter_by(email=body['email']).first()
+
+    if user is None:
+        return jsonify({'msg': 'Usuario o contraseña errónea'}), 400
+    
+
+    print(user.password)
+    
+    password_correct = bcrypt.check_password_hash(
+        user.password, body['password'])  # returns True
+    
+    if not password_correct:
+        return jsonify({'msg': 'Usuario o contraseña errónea'}), 400
+    
+    acces_token = create_access_token(identity=user.email)
+    return jsonify({'msg': 'OK', 'Token': acces_token}),200
+
+# -------------------------------PROTECCIÓN ---OK--------------------------------
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity() # sabemos quien es el usuario que hace la petición
+    return jsonify({'msg': f'Acceso del usuario {current_user_id} ACEPTADO'}), 200
+
+
+# ------------------------------- ROLE_REQUIRED ------------------------------------
+
+
+def role_required(*roles):
+    def wrapper(fn):
+        @wraps(fn)
+        @jwt_required()
+        def decorator (*args, **kwargs):
+            current_user_email = get_jwt_identity()
+            user = User.query.filter_by(email = current_user_email).first()
+            if not user or user.rol not in roles:
+                return jsonify({'msg':'Acceso denegado'}), 400
+            return fn(*args, *kwargs)
+        return decorator
+    return wrapper
+
+
+
+
+
+# ----------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
