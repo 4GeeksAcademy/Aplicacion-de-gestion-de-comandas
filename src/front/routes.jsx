1869ad75475001a4dc49f92cc5e3d2a57f74e62b
Grin <martanvcs@gmail.com>
2025-07-09 18:38:34 +0000

    añadida la ruta en roustes de kitchen

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,18 +9,26 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import Login from "./pages/Login";
 import Private from "./pages/Private";
-import OrdersDashboard from "./pages/OrdersDashboard";
 import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
 import RequestReset from "./pages/RequestReset";
 import TableMap from "./pages/TableMap";
+import OrdersDashboard from "./pages/OrdersDashboard";
+
+
 
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      {/* 🌐 RUTA PRINCIPAL con Layout */}
-    
+    // CreateRoutesFromElements function allows you to build route elements declaratively.
+    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
+    // Root, on the contrary, create a sister Route, if you have doubts, try it!
+    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
+    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+
+    // Root Route: All navigation will start from here.
+    <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
+
       {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
       <Route path="/" element={<Login />} />
       <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
@@ -32,7 +40,8 @@ export const router = createBrowserRouter(
       <Route path="/request-reset-password" element={<RequestReset />} />
       <Route path="/table-map" element={<TableMap/>} />
       <Route path="/orders-dashboard" element={<OrdersDashboard />} />
-    </>
+      
+    </Route>
   ),
 
   
